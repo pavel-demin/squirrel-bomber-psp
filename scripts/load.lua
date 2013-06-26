b8a34bfec46531df8ab84159e91f0cfd12e9cb6f
kpereksta <kpereksta@6ad07be7-ac69-11e6-1fbd-5585dccdc8cf>
2013-06-26 08:00:46 +0000

    Renamed load_splashes.lua to loadscreen.lua and moved to classes folder Fixed some issues.

--- scripts/load.lua
+++ scripts/load.lua
@@ -1,11 +1,20 @@
-dofile('scripts/load_splashes.lua')
+dofile('scripts/classes/loadscreen.lua')
 
 --Competition rules require Neoflash splash screen to come first :P lame...will add once i finish splash class
 os.luadevsplash()
 
 splashtest = LoaderScreen.create(0)
+
+splashtest:setFadeSpeed(5)
+
 splashtest:setSplash(image.load('images/splash/neosplash.png'))
+splashtest:fadeOut()
+
 for x=1000,0,-5 do
- -- hack stuff
+ -- hack stuff 
+ --Er... What stuff?
 end
 splashtest:setSplash(image.load('images/splash/game_splash.png'))
+splashtest:fadeOut()
+
+splashtest:clearSplash()
