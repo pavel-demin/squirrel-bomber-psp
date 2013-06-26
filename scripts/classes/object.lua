9fc1475012cb26495ff2151ea725393e7ea89703
kpereksta <kpereksta@6ad07be7-ac69-11e6-1fbd-5585dccdc8cf>
2013-06-26 04:49:52 +0000

    Quick update. Overlooked a problem in model loading. None of these classes have been tested, but they should work... I think...

--- scripts/classes/object.lua
+++ scripts/classes/object.lua
@@ -12,6 +12,10 @@ function Object.create(Model, Vector3, Collider)
 	return obj
 end
 
+function Object:update()
+	self.model:position(obj.position:getPosition())
+	self.model:rotation(obj.position:getRotation())
+end
 
 
 --[[
@@ -30,6 +34,12 @@ newObject.position:setRotation(rotX, rotY, rotZ)
 
 We can check for collisions with other objects like this:
 newObject.collider:checkCollision(otherObject.collider)
+
+--When we want to blit them we do:
+newObject:update()
+newObject.model:blit()
+
+So clean... So pretty...
 
 Pretty awesome, no?
 
