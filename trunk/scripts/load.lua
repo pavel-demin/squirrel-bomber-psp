23f93492e391b21bc40419f7893df29a53669ad9
andreyrd1@gmail.com <andreyrd1@gmail.com@6ad07be7-ac69-11e6-1fbd-5585dccdc8cf>
2013-06-27 02:41:00 +0000

    finished the loading bar...onto some animatioN?

--- trunk/scripts/load.lua
+++ trunk/scripts/load.lua
@@ -8,13 +8,22 @@ splashtest:setSplash(image.load('images/splash/neosplash.png'))
 
 --Load Classes:
 dofile("scripts/classes/vector.lua")
+splashtest:setPercent(5)
 dofile("scripts/classes/collisiondata.lua")
+splashtest:setPercent(15)
 dofile("scripts/classes/object.lua")
+splashtest:setPercent(30)
 dofile("scripts/classes/camera.lua")
+splashtest:setPercent(40)
 dofile("scripts/classes/light.lua")
+splashtest:setPercent(50)
 dofile("scripts/classes/cameramanager.lua")
+splashtest:setPercent(55)
 dofile("scripts/classes/lightmanager.lua")
+splashtest:setPercent(60)
 dofile("scripts/classes/environment.lua")
+splashtest:setPercent(70)
+
 
 --The crossfade doesn't look so hot :/
 splashtest:crossFade(image.load('images/splash/game_splash.png'))
