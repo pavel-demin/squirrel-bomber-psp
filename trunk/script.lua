61b0c7836780d7826b44a430f904395492112931
kpereksta <kpereksta@6ad07be7-ac69-11e6-1fbd-5585dccdc8cf>
2013-06-27 04:42:30 +0000

    Added sine Docs and Menu functions

--- trunk/script.lua
+++ trunk/script.lua
@@ -5,16 +5,18 @@ mainmenu:addItem('Test', 'scripts/test.lua')
 mainmenu:addItem('Test2', 'scripts/test2.lua')
 mainmenu:addItem('Test3', 'scripts/test3.lua')
 
+math.atan2(90 - 60, 130 - 30)
+
 while true do
 	controls.read()
 	
-	if controls.up() then
+	if controls.pressed("up") then
 		mainmenu:moveUp()
 	end
-	if controls.down() then
+	if controls.pressed("down") then
 		mainmenu:moveDown()
 	end
-	if controls.cross() then
+	if controls.pressed("cross") then
 		mainmenu:executeSelected()
 	end
 	
--- trunk/scripts/classes/menu.lua
+++ trunk/scripts/classes/menu.lua
@@ -9,7 +9,31 @@ function Menu.create(id)
 	menu.Items = {}
 	menu.selected = 1
 	
+	menu.textSpacing = 15
+	menu.center = false
+	
+	menu.x = 0
+	menu.y = 0
+	
+	menu.font = nil
+	
+	menu.selectedColor = color.new(0, 255, 0)
+	menu.unselectedColor = color.new(255, 255, 255)
+	
 	return menu
+end
+
+function Menu:setPosition(x, y)
+	self.x = x
+	self.y = y
+end
+
+function Menu:setSelectedColor(color)
+	self.selectedColor = color
+end
+
+function Menu:setUnselectedColor(color)
+	self.unselectedColor = color
 end
 
 function Menu:addItem(title, script)
@@ -22,9 +46,11 @@ end
 
 function Menu:drawFrame() -- Draws one frame of our menu
 	for a=1, #self.Items, 1 do
-		screen.print(0, a*15+5, self.Items[a][1], color.new(255,255,255))
+		if a ~= self.selected then
+			screen.print(self.x, self.y + a * self.textSpacing - self.textSpacing, self.Items[a][1], self.unselectedColor)
+		end
 	end
-	screen.print(0, (self.selected)*15+5, self.Items[self.selected][1], color.new(0,255,0))
+	screen.print(self.x, (self.selected)*self.textSpacing + 5, self.Items[self.selected][1], self.selectedColor)
 end
 
 function Menu:moveUp() -- Call this when you want to move up
