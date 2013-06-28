69b2815a6231b3c536df10deb143be2a5386b78c
kpereksta <kpereksta@6ad07be7-ac69-11e6-1fbd-5585dccdc8cf>
2013-06-28 20:19:06 +0000

    Scaled the models and put scale information in objects/loadobjects.lua

---
+++ objects/loadobjects.lua
@@ -0,0 +1,15 @@
+--Contains model data for all of our objects:
+
+--[[
+	Scales:
+	
+	Squirrel: 2 x 3.5 x 5 (Current)
+	
+	Acorn: 1x1x1 (Current)
+	
+	Buckeye: 1.2 x 1.2 x 1.2
+	
+	Tree Base: 10 x 30 x 10
+	
+	Forest Floor 125 x 0 x 125
+]]--
