208812b8da3156c94ab140f8092cc9281adbc5e3
kpereksta <kpereksta@6ad07be7-ac69-11e6-1fbd-5585dccdc8cf>
2013-07-03 01:03:47 +0000

    Fixed the Nest.3ds Added FPSController (Working :D) Added Nest example (Needs either more subdivided floors, or raised camera)
    Added an example song by my brother (Awesome, no?)

--- scripts/classes/camera.lua
+++ scripts/classes/camera.lua
@@ -9,6 +9,8 @@ function Camera.create()
 	setmetatable(cam, Camera)
 	
 	cam.position = Vector.create({0, 0, 0}, {0, 0, 0})
+	cam.position:setRotation(0, 0, 0)
+	cam.position:setPosition(0, 0, 0)
 	cam.lookAt = {0, 0, 1}
 	cam.up = {0, 1, 0}
 	return cam
@@ -18,7 +20,6 @@ function Camera:lookAtRotation()
 	self.lookAt[1] = self.position.position[1] + math.cos(math.rad(self.position.rotation[1])) * 2
 	self.lookAt[3] = self.position.position[3] + math.sin(math.rad(self.position.rotation[1])) * 2
 	self.lookAt[2] = self.position.position[2] + math.cos(math.rad(self.position.rotation[2])) * 2
-	
 end
 
 function Camera:lookAtPosition(vector)
