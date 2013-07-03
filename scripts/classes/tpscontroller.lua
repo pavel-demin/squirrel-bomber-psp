13070b67abbd2ea5be7f9aa5b25bab590dce660e
kpereksta <kpereksta@6ad07be7-ac69-11e6-1fbd-5585dccdc8cf>
2013-07-03 04:04:12 +0000

    Added a Third Person Controller and updated the world_forest1.lua

---
+++ scripts/classes/tpscontroller.lua
@@ -0,0 +1,64 @@
+TPSController = {}
+TPSController.__index = TPSController
+
+function TPSController.create(model)
+	local tps = {}
+	setmetatable(tps, TPSController)
+	
+	
+	tps.object = Object.create(model)
+	
+	tps.camera = Camera.create()
+	tps.camera.position:setPosition(1, 13, 0)
+	tps.cameraCollider = CollisionData.create()
+	
+	tps.moveSpeed = 5
+	tps.cameraSpeed = 0.12
+	
+	return tps
+end
+
+
+function TPSController:setModeSpeed(speed)
+	self.moveSpeed = speed
+end
+
+function TPSController:setCameraSpeed(speed)
+	self.cameraSpeed = speed
+end
+
+function TPSController:update()
+	
+	--Fix Camera Angle:
+	self.camera.position:rotateTowards(self.object.position)
+	self.camera:lookAtPosition(self.object.position)
+	
+	--Move camera to player, if necessary
+	--Move Cam towards Player
+	local Dist = self.object.position:getDistanceTo(self.camera.position)
+	if Dist > 17 then	
+		self.camera.position:moveTowards(self.object.position, self.cameraSpeed, {true, false, true})
+	end
+	
+	--Movement
+	CAngle = self.camera.position.rotation
+	
+	if controls.left() then
+		self.object.position:setPosition(self.object.position.position[1] + math.cos(CAngle[1] + math.rad(270)) / 5, self.object.position.position[2], self.object.position.position[3] + math.sin(CAngle[1] + math.rad(270)) / 5)
+	end
+	if controls.right() then
+		self.object.position:setPosition(self.object.position.position[1] + math.cos(CAngle[1] + math.rad(90)) / 5, self.object.position.position[2], self.object.position.position[3] + math.sin(CAngle[1] + math.rad(90)) / 5)
+	end
+	if controls.up() then
+		self.object.position:setPosition(self.object.position.position[1] + math.cos(CAngle[1]) / 5, self.object.position.position[2], self.object.position.position[3] + math.sin(CAngle[1]) / 5)
+	end
+	if controls.down() then
+		self.object.position:setPosition(self.object.position.position[1] + math.cos(CAngle[1] + math.rad(180)) / 5, self.object.position.position[2], self.object.position.position[3] + math.sin(CAngle[1] + math.rad(180)) / 5)
+	end
+	if controls.l() then
+		Cam.position:setPosition(Cam.position.position[1] + math.cos(CAngle[1] + math.rad (270)) / 5, Cam.position.position[2], Cam.position.position[3] + math.sin(CAngle[1] + math.rad(270)) / 5)
+	end
+	if controls.r() then
+		Cam.position:setPosition(Cam.position.position[1] + math.cos(CAngle[1] + math.rad (90)) / 5, Cam.position.position[2], Cam.position.position[3] + math.sin(CAngle[1] + math.rad(90)) / 5)
+	end
+end
--- scripts/world_forest1.lua
+++ scripts/world_forest1.lua
@@ -1,19 +1,13 @@
 --[[ This is Messy. It needs organized ]]--
 
-rad = math.rad
-
 forest1splash = LoaderScreen.create(0)
 forest1splash:setFadeSpeed(8.5)
 forest1splash:setSplash(image.load('images/splash/game_splash.png'))
 forest1splash:setObjects(27)
-
-Cam = Camera.create()
-	forest1splash:incrementPercent()
-Cam.position:setPosition(1, 13, 0)
-	forest1splash:incrementPercent()
+inc = forest1splash:incrementPercent
 
 TreeModel = model.load("objects/treebase.obj", 0.15, color.new(0, 0, 0))
-	forest1splash:incrementPercent()
+	inc()
 TreeMap = {{-30,0,0},{30,0,0},{0,0,30}}
 Trees = {}
 
@@ -23,59 +17,57 @@ for a = 1, #TreeMap, 1 do
 	Trees[a].collider:setData({{-(10)/2, 0, -(10)/2}, {10, 10, 10}})
 	Trees[a]:update()
 end
-	forest1splash:incrementPercent()
+	inc()
 
 Floor = Object.create(model.load("objects/forestfloor.obj"))
-	forest1splash:incrementPercent()
+	inc()
 Floor.position:setPosition(2, 0, 0)
-	forest1splash:incrementPercent()
+	inc()
 Floor:update()
-	forest1splash:incrementPercent()
-	
-AcornObject = Object.create(model.load("objects/acorn.obj", 0.15, color.new(0, 0, 0)))
-	forest1splash:incrementPercent()
-AcornObject.collider:setData({{-(2.255)/2, 0, -(2.255)/2}, {2.255, 2.255, 2.255}})
-	forest1splash:incrementPercent()
-AcornObject.position:setPosition(0, 0, 0)
-	forest1splash:incrementPercent()
-AcornObject:update()
-	forest1splash:incrementPercent()
+	inc()
+
+Player = TPSController.create(model.load("objects/acorn.obj", 0.15, color.new(0, 0, 0)))
+	inc()
+Player.object.collider:setData({{-(2.255)/2, 0, -(2.255)/2}, {2.255, 2.255, 2.255}})
+	inc()
+Player.object:update()
+	inc()
 	
 Atmosphere = Environment.create()
-	forest1splash:incrementPercent()
+	inc()
 Atmosphere:setLights(true) --Enables lights
-	forest1splash:incrementPercent()
+	inc()
 Atmosphere:setAmbient(color.new(255, 255, 255)) --Sets the ambient color
-	forest1splash:incrementPercent()
+	inc()
 Atmosphere:setSpecular(4.04) --sets the specular strength
-	forest1splash:incrementPercent()
-Atmosphere:setFogNear(40) --Sets where fog starts
-	forest1splash:incrementPercent()
-Atmosphere:setFogFar(50) --And where it ends
-	forest1splash:incrementPercent()
-Atmosphere:setFogColor(color.new(1, 102, 0.5)) --And the color of fog ?? WHITE?
-	forest1splash:incrementPercent()
+	inc()
+Atmosphere:setFogNear(20) --Sets where fog starts
+	inc()
+Atmosphere:setFogFar(30) --And where it ends
+	inc()
+Atmosphere:setFogColor(color.new(0, 0, 20)) --And the color of fog ?? WHITE?
+	inc()
 Atmosphere:update()
-	forest1splash:incrementPercent()
+	inc()
 
 light = Light.create()
 light:setId(1)
-	forest1splash:incrementPercent()
+	inc()
 light:enable()
-	forest1splash:incrementPercent()
+	inc()
 light:setType(2)
-	forest1splash:incrementPercent()
+	inc()
 light:setDiffuse(color.new(0,0,0))
-	forest1splash:incrementPercent()
+	inc()
 light:setAmbient(color.new(255,255,255))
-	forest1splash:incrementPercent()
+	inc()
 light.position:setPosition(0,5,0)
-	forest1splash:incrementPercent()
+	inc()
 light:update()
-	forest1splash:incrementPercent()
+	inc()
 
 controls.analogtodigital(50)
-	forest1splash:incrementPercent()
+	inc()
 	
 forest1splash:fadeOut()
 forest1splash:clearSplash()
@@ -87,59 +79,30 @@ while true do
 	
 	world.update()
 	
-	Cam.position:rotateTowards(AcornObject.position) --I'm using this to store the rotation
+	Player.camera:setView()
+	Player:update()
 	
-	Cam:lookAtPosition(AcornObject.position)
-	Cam:setView()
+	Player.object:update()
+	Player.object.model:blit()
+	
+	APos = Player.object.position.position
+	BPos = Player.camera.position.position
 	
 	Floor:update()
 	Floor.model:blit()
-	
-	AcornObject:update()
-	AcornObject.model:blit()
-	
-	APos = AcornObject.position.position
-	BPos = Cam.position.position
-	
-	--Move Cam towards Player
-	Dist = AcornObject.position:getDistanceTo(Cam.position)
-	if Dist > 17 then	
-		Cam.position:moveTowards(AcornObject.position, 0.15, {true, false, true})
-	end
 	
 	for a = 1, #Trees, 1 do
 		Trees[a]:update()
 		Trees[a].model:blit()
 	end
-	
-	CAngle = Cam.position.rotation
-	CAngle[1] = CAngle[1]
-	
-	if controls.left() then
-		AcornObject.position:setPosition(AcornObject.position.position[1] + math.cos(CAngle[1] + rad(270)) / 5, AcornObject.position.position[2], AcornObject.position.position[3] + math.sin(CAngle[1] + rad(270)) / 5)
-	end
-	if controls.right() then
-		AcornObject.position:setPosition(AcornObject.position.position[1] + math.cos(CAngle[1] + rad(90)) / 5, AcornObject.position.position[2], AcornObject.position.position[3] + math.sin(CAngle[1] + rad(90)) / 5)
-	end
-	if controls.up() then
-		AcornObject.position:setPosition(AcornObject.position.position[1] + math.cos(CAngle[1]) / 5, AcornObject.position.position[2], AcornObject.position.position[3] + math.sin(CAngle[1]) / 5)
-	end
-	if controls.down() then
-		AcornObject.position:setPosition(AcornObject.position.position[1] + math.cos(CAngle[1] + rad(180)) / 5, AcornObject.position.position[2], AcornObject.position.position[3] + math.sin(CAngle[1] + rad(180)) / 5)
-	end
-	if controls.l() then
-		Cam.position:setPosition(Cam.position.position[1] + math.cos(CAngle[1] + rad (270)) / 5, Cam.position.position[2], Cam.position.position[3] + math.sin(CAngle[1] + rad(270)) / 5)
-	end
-	if controls.r() then
-		Cam.position:setPosition(Cam.position.position[1] + math.cos(CAngle[1] + rad (90)) / 5, Cam.position.position[2], Cam.position.position[3] + math.sin(CAngle[1] + rad(90)) / 5)
 		
-	end
-	
 	screen.print(0, 2, "FPS: " .. screen.fps(), color.new(255, 255, 255))
-	screen.print(0, 16, "Distance to Player: " .. Dist, color.new(255, 255, 255))
-	screen.print(0, 32, "Player: (" .. APos[1] .. ", " .. APos[2] .. ", " .. APos[3] .. ")", color.new(255, 255, 255))
-	screen.print(0, 48, "Camera: (" .. BPos[1] .. ", " .. BPos[2] .. ", " .. BPos[3] .. ")", color.new(255, 255, 255))
-	screen.print(0, 64, "Angle to player: " .. math.deg(CAngle[1]), color.new(255, 255, 255))
+	screen.print(0, 16, "Player: (" .. APos[1] .. ", " .. APos[2] .. ", " .. APos[3] .. ")", color.new(255, 255, 255))
+	screen.print(0, 30, "Camera: (" .. BPos[1] .. ", " .. BPos[2] .. ", " .. BPos[3] .. ")", color.new(255, 255, 255))
+	
+	if controls.select() then
+		error("USB Mode")
+	end
 	
 	screen.flip()
 	screen.waitvblank()
