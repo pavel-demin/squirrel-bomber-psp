75f3066cf56ccde061317361da3c467d75aadbcd
andreyrd1@gmail.com <andreyrd1@gmail.com@6ad07be7-ac69-11e6-1fbd-5585dccdc8cf>
2013-07-07 22:23:30 +0000

    decent collision "sliding"

--- trunk/scripts/classes/tpscontroller.lua
+++ trunk/scripts/classes/tpscontroller.lua
@@ -13,11 +13,9 @@ function TPSController.create(model)
 	tps.cameraCollider = CollisionData.create()
 	
 	tps.moveSpeed = 5
-	tps.cameraSpeed = 0.12
+	tps.cameraSpeed = 0.14
 	
 	tps.colliders = {}
-	
-	tps.collision = false
 	
 	tps.fw = 0.5
 	tps.bw = 0.5
@@ -57,25 +55,47 @@ function TPSController:addCollider(col)
 	table.insert(self.colliders, col)
 end
 
-function TPSController:collisionStuff() -- To keep it organized, since we have it in all 6 control checks ;)
+function TPSController:collisionCheck()
+	collision = false
 	self.object.collider:findExtreme(self.object.position, self.fw, self.bw, self.rw, self.lw)
-	self.collision = false -- Redundant but oh well :P
 	for a = 1, #self.colliders do
 		if self.colliders[a].Type == 1 then
 			if self.object.collider:checkCollision(self.colliders[a]) then
-				self.collision = true
+				collision = true
 			end
 		else
 			for b = 1, 4 do
 				if self.colliders[a]:capsuleCollision(self.object.collider.extreme[b]) then
-					self.collision = true
+					collision = true
 					break
 				end
 			end
 		end
 	end
-	if self.collision then
+	return collision
+end
+
+function TPSController:collisionAct()
+	if self:collisionCheck() then
 		self.object.position:stepBack({true, false})
+		for a = 1, 6, 1 do
+			self.object.position:setPosition(self.object.position.position[1] + math.cos(CAngle[1] + math.rad(a * 15)) / 5,
+											 self.object.position.position[2],
+											 self.object.position.position[3] + math.sin(CAngle[1] + math.rad(a * 15)) / 5)
+			if self:collisionCheck() then
+				self.object.position:stepBack({true, false})
+			else
+				break
+			end
+			self.object.position:setPosition(self.object.position.position[1] + math.cos(CAngle[1] + math.rad(a * -15)) / 5,
+											 self.object.position.position[2],
+											 self.object.position.position[3] + math.sin(CAngle[1] + math.rad(a * -15)) / 5)
+			if self:collisionCheck() then
+				self.object.position:stepBack({true, false})
+			else
+				break
+			end
+		end
 		self.object:update()
 	end
 end
@@ -99,56 +119,54 @@ function TPSController:update()
 	local Delta = Vector.create()
 	Delta:setPosition(self.object.position.position[1], self.object.position.position[2], self.object.position.position[3])
 	
-	self.collision = false
-	
-	if controls.up() and controls.left() then
-		self.object.position:setPosition(self.object.position.position[1] + (math.cos(CAngle[1]) / 5 + math.cos(CAngle[1] + math.rad(270)) / 5) * (math.sqrt(2)/2),
-										 self.object.position.position[2],
-										 self.object.position.position[3] + (math.sin(CAngle[1]) / 5 + math.sin(CAngle[1] + math.rad(270)) / 5) * (math.sqrt(2)/2))
-		self.object:update()
-		self:collisionStuff()
-	elseif controls.up() and controls.right() then
+	if controls.up() and controls.right() then
 		self.object.position:setPosition(self.object.position.position[1] + (math.cos(CAngle[1]) / 5 + math.cos(CAngle[1] + math.rad(90)) / 5) * (math.sqrt(2)/2),
 										 self.object.position.position[2],
 										 self.object.position.position[3] + (math.sin(CAngle[1]) / 5 + math.sin(CAngle[1] + math.rad(90)) / 5) * (math.sqrt(2)/2))
 		self.object:update()
-		self:collisionStuff()
-	elseif controls.down() and controls.left() then
-		self.object.position:setPosition(self.object.position.position[1] + (math.cos(CAngle[1] + math.rad(180)) / 5 + math.cos(CAngle[1] + math.rad(270)) / 5) * (math.sqrt(2)/2),
-										 self.object.position.position[2],
-										 self.object.position.position[3] + (math.sin(CAngle[1] + math.rad(180)) / 5 + math.sin(CAngle[1] + math.rad(270)) / 5) * (math.sqrt(2)/2))
-		self.object:update()
-		self:collisionStuff()
+		self:collisionAct()
 	elseif controls.down() and controls.right() then
 		self.object.position:setPosition(self.object.position.position[1] + (math.cos(CAngle[1] + math.rad(180)) / 5 + math.cos(CAngle[1] + math.rad(90)) / 5) * (math.sqrt(2)/2),
 										 self.object.position.position[2],
 										 self.object.position.position[3] + (math.sin(CAngle[1] + math.rad(180)) / 5 + math.sin(CAngle[1] + math.rad(90)) / 5) * (math.sqrt(2)/2))
 		self.object:update()
-		self:collisionStuff()
-	elseif controls.left() then
-		self.object.position:setPosition(self.object.position.position[1] + math.cos(CAngle[1] + math.rad(270)) / 5,
-										self.object.position.position[2],
-										self.object.position.position[3] + math.sin(CAngle[1] + math.rad(270)) / 5)
+		self:collisionAct()
+	elseif controls.down() and controls.left() then
+		self.object.position:setPosition(self.object.position.position[1] + (math.cos(CAngle[1] + math.rad(180)) / 5 + math.cos(CAngle[1] + math.rad(270)) / 5) * (math.sqrt(2)/2),
+										 self.object.position.position[2],
+										 self.object.position.position[3] + (math.sin(CAngle[1] + math.rad(180)) / 5 + math.sin(CAngle[1] + math.rad(270)) / 5) * (math.sqrt(2)/2))
 		self.object:update()
-		self:collisionStuff()
-	elseif controls.right() then
-		self.object.position:setPosition(self.object.position.position[1] + math.cos(CAngle[1] + math.rad(90)) / 5,
-										self.object.position.position[2],
-										self.object.position.position[3] + math.sin(CAngle[1] + math.rad(90)) / 5)
+		self:collisionAct()
+	elseif controls.up() and controls.left() then
+		self.object.position:setPosition(self.object.position.position[1] + (math.cos(CAngle[1]) / 5 + math.cos(CAngle[1] + math.rad(270)) / 5) * (math.sqrt(2)/2),
+										 self.object.position.position[2],
+										 self.object.position.position[3] + (math.sin(CAngle[1]) / 5 + math.sin(CAngle[1] + math.rad(270)) / 5) * (math.sqrt(2)/2))
 		self.object:update()
-		self:collisionStuff()
+		self:collisionAct()
 	elseif controls.up() then
 		self.object.position:setPosition(self.object.position.position[1] + math.cos(CAngle[1]) / 5,
 										self.object.position.position[2],
 										self.object.position.position[3] + math.sin(CAngle[1]) / 5)
 		self.object:update()
-		self:collisionStuff()
+		self:collisionAct()
+	elseif controls.right() then
+		self.object.position:setPosition(self.object.position.position[1] + math.cos(CAngle[1] + math.rad(90)) / 5,
+										self.object.position.position[2],
+										self.object.position.position[3] + math.sin(CAngle[1] + math.rad(90)) / 5)
+		self.object:update()
+		self:collisionAct()
 	elseif controls.down() then
 		self.object.position:setPosition(self.object.position.position[1] + math.cos(CAngle[1] + math.rad(180)) / 5,
 										self.object.position.position[2],
 										self.object.position.position[3] + math.sin(CAngle[1] + math.rad(180)) / 5)
 		self.object:update()
-		self:collisionStuff()
+		self:collisionAct()
+	elseif controls.left() then
+		self.object.position:setPosition(self.object.position.position[1] + math.cos(CAngle[1] + math.rad(270)) / 5,
+										self.object.position.position[2],
+										self.object.position.position[3] + math.sin(CAngle[1] + math.rad(270)) / 5)
+		self.object:update()
+		self:collisionAct()
 	end
 	
 	if controls.l() then
