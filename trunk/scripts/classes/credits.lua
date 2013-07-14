dc9231ec423954eace4f0ac7906bcf2e3738b0b1
andreyrd1@gmail.com <andreyrd1@gmail.com@6ad07be7-ac69-11e6-1fbd-5585dccdc8cf>
2013-07-14 05:42:31 +0000

    fixed the credits (hopefully) :D

--- trunk/scripts/classes/credits.lua
+++ trunk/scripts/classes/credits.lua
@@ -12,7 +12,7 @@ function Credits.create()
 	credits.align = 2 -- 1-left, 2-center, 3-right
 	credits.direction = -1 -- negative-down, positive-up
 	credits.posY = 0
-	credits.lineSpacing = 2
+	credits.lineSpacing = 3
 	credits.marginLeft = 5 -- Margins only affect left/right align
 	credits.marginRight = 5
 	
@@ -20,7 +20,11 @@ function Credits.create()
 end
 
 function Credits:calcInitPosY()
-	self.posY = ((#self.Items * 15) + ((#self.Items - 1) * self.lineSpacing)) * self.direction
+	if self.direction == -1 then
+		self.posY = ((#self.Items * 14) + ((#self.Items - 1) * self.lineSpacing)) * -1
+	else
+		self.posY = 272
+	end
 end
 
 function Credits:setAlign(align) -- 1-left, 2-center, 3-right
@@ -58,13 +62,11 @@ function Credits:addItem(text)
 	-- Please add/remove all items before doing any scrolling
 	-- I'm going to eventually add support for mid-scroll changes
 	table.insert(self.Items, text)
-	self:calcInitPosY()
 	return #self.Items
 end
 
 function Credits:removeItem(id)
 	table.remove(self.Items, id or #self.Items)
-	self:calcInitPosY()
 	return #self.Items
 end
 
@@ -83,20 +85,21 @@ function Credits:drawFrame()
 		if (self.align == 3) then
 			posX = 480 - screen.textwidth(self.Items[a]) - self.marginRight
 		end
-		screen.print(posX, self.posY + (a * 15) + (a-1) * self.lineSpacing, self.Items[a])
+		screen.print(posX, self.posY + (a * 14) + (a-1) * self.lineSpacing, self.Items[a])
 	end
 end
 
 function Credits:doneScrolling()
-	if direction == 1 then
-		if self.posY < (-1 * ((#self.Items * 15) + ((#self.Items - 1) * self.lineSpacing))) then
-			return true
+	x = false
+	if self.direction == 1 then
+		if self.posY < (-1 * ((#self.Items * 14) + ((#self.Items - 1) * self.lineSpacing))) then
+			x = true
 		end
 	end
-	if direction == -1 then
-		if self.posY > (480 + (#self.Items * 15) + ((#self.Items - 1) * self.lineSpacing)) then
-			return true
+	if self.direction == -1 then
+		if self.posY < (480 + (#self.Items * 14) + ((#self.Items - 1) * self.lineSpacing)) then
+			x = true
 		end
 	end
-	return false
+	return x
 end
