162658f679d1162ef341c30f747d0be2cef5aba3
kpereksta <kpereksta@6ad07be7-ac69-11e6-1fbd-5585dccdc8cf>
2013-07-16 23:51:05 +0000

    Fixed FPS Controller maybe.

--- trunk/scripts/classes/fpscontroller.lua
+++ trunk/scripts/classes/fpscontroller.lua
@@ -3,45 +3,82 @@ FPSController.__index = FPSController
 
 function FPSController.create()
 	local fps = {}
-	setmetatable(fps, TPSController)
-	
-	fps.camera = Camera.create()
-	fps.collider = CollisionData.create()
-	
-	fps.moveSpeed = 5
-	fps.lookSpeed = 0.5
-	
-	fps.surround = CollisionData.create()
-	fps.collisions = {}
+	setmetatable(fps, FPSController)
 	
 	fps.keys = Keys.create()
 	
-	return tps
+	fps.camera = Camera.create()
+	fps.position = Vector.create()
+	fps.position:setPosition(0, 0, 0)
+	fps.position:setRotation(0, 0, 0)
+	fps.collider = CollisionData.create()
+	fps.collider:setData({{-1, -1, -1}, {1, 1, 1}})
+	fps.collisionObjects = {}
+	fps.bounds = 10
+	fps.moveSpeed = 0.1
+	fps.lookSpeed = 3
+	
+	return fps
 end
 
-function FPSController:addCollider(col)
-	table.insert(self.collisions, col)
+function FPSController:addCollisionObject(obj)
+	table.insert(self.collisionObjects, obj)
 end
 
-function FPSController:setModeSpeed(speed)
+function FPSController:setCollisionObjects(objs)
+	for a = 1, #objs do
+		table.insert(self.collisionobjects, objs[a])
+	end
+end
+
+function FPSController:setMoveSpeed(speed)
 	self.moveSpeed = speed
 end
 
+function FPSController:setLookSpeed(speed)
+	self.lookSpeed = speed
+end
+
+function FPSController:setBounds(bound)
+	self.bounds = bound
+end
+
 function FPSController:update()
-	--Look:
+	--First get lookat
+	self.camera.position:setPosition(self.position.position[1], self.position.position[2], self.position.position[3])
+	self.camera.position:setRotation(self.position.rotation[1], self.position.rotation[2], 0)
+	self.camera:lookAtRotation()
+	
+	--Check controls.
 	if self.keys.left() then
-		self.camera.position.rotation:addToRotation(0, -self.lookSpeed, 0)
+		self.position.rotation[1] = self.position.rotation[1] - self.lookSpeed
 	end
 	if self.keys.right() then
-		self.camera.position.rotation:addToRotation(0, self.lookSpeed, 0)
+		self.position.rotation[1] = self.position.rotation[1] + self.lookSpeed
 	end
+	
 	if self.keys.up() then
-		self.camera.position.rotation:addToRotation(self.lookSpeed, 0, 0)
+		self.position.rotation[2] = self.position.rotation[2] - self.lookSpeed
 	end
 	if self.keys.down() then
-		self.camera.position.rotation:addToRotation(-self.lookSpeed, 0, 0)
+		self.position.rotation[2] = self.position.rotation[2] + self.lookSpeed
 	end
 	
-	--Move:
+	--Now I know :P
+	local PPos = self.position.position
+	local PRot = self.position.rotation
 	
+	if self.keys.triangle() then
+		self.position:setPosition(PPos[1] + math.cos(math.rad(PRot[1])) * self.moveSpeed, PPos[2], PPos[3] + math.sin(math.rad(PRot[1])) * self.moveSpeed)
+	end
+	if self.keys.cross() then
+		self.position:setPosition(PPos[1] + math.cos(math.rad(PRot[1] + 180)) * self.moveSpeed, PPos[2], PPos[3] + math.sin(math.rad(PRot[1] + 180)) * self.moveSpeed)
+	end
+	
+	if self.keys.circle() then
+		self.position:setPosition(PPos[1] + math.cos(math.rad(PRot[1] + 90)) * self.moveSpeed, PPos[2], PPos[3] + math.sin(math.rad(PRot[1] + 90)) * self.moveSpeed)
+	end
+	if self.keys.circle() then
+		self.position:setPosition(PPos[1] + math.cos(math.rad(PRot[1] + 270)) * self.moveSpeed, PPos[2], PPos[3] + math.sin(math.rad(PRot[1] + 270)) * self.moveSpeed)
+	end
 end
